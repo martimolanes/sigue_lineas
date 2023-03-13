8ef4bfcd8d323f83b127abc72e944a3c6d95584c
martinho <martinho.n03@gmail.com>
2023-03-13 23:59:04 +0100

    added points and updated the size of the window

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
+import random as rd
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QBrush, QPainter, QPen
+from PyQt5.QtGui import QBrush, QPainter, QPen, QColor
 from PyQt5.QtWidgets import (
     QApplication,
     QGraphicsEllipseItem,
@@ -41,21 +42,20 @@ class Car(QGraphicsRectItem):
         top_right = self.x() + width * np.cos(theta_rad), self.y() + width * np.sin(theta_rad)
         bottom_left = self.x() + height * np.sin(theta_rad), self.y() +height * np.cos(theta_rad)
         bottom_right = self.x() + (width * np.cos(theta_rad) - height * np.sin(theta_rad)), self.y() + width * np.sin(theta_rad) + height * np.cos(theta_rad)
-        print(top_left, top_right, bottom_right, bottom_left)
 
         return [top_left, top_right, bottom_left, bottom_right]
 
     def move_forward(self, dist, corners):
-        if min_x(corners) < 0:
+        if min_x(corners) < -30:
             self.setRotation((180 - self.rotation()) % 360)
             self.setX(max_x(corners))
-        if max_x(corners) > 400:
+        if max_x(corners) > 800:
             self.setRotation((180 - self.rotation()) % 360)
             self.setX(min_x(corners))
-        if min_y(corners) < 0:
+        if min_y(corners) < -30:
             self.setRotation((360 - self.rotation()) % 360)
             self.setY(max_y(corners))
-        if max_y(corners) > 200:
+        if max_y(corners) > 500:
             self.setRotation((360 - self.rotation()) % 360)
             self.setY(min_y(corners))
         
@@ -80,7 +80,6 @@ def max_x(corners):
     max = -np.Inf
     for corner in corners:
         if corner[0] > max:
-            print(str(corner[0]))
             max = corner[0]
     return max
 
@@ -104,10 +103,21 @@ class CarTrack(QWidget):
         super().__init__()
 
         # Defining a scene rect of 400x200
-        self.scene = QGraphicsScene(0, 0, 400, 200)
+        self.scene = QGraphicsScene(0, 0, 800, 500)
 
         # Draw a Car
         car = Car(30, 30)
+
+        pen = QPen(QColor("black"), 4)
+
+        coordinates = []
+
+        # create a small ellipse item to simulate a point
+        for i in range(12):
+            random_x = rd.randint(25, 775)
+            random_y = rd.randint(25, 475)
+            point = self.scene.addEllipse(random_x, random_y, 1, 1, pen)
+            coordinates.append((random_x, random_y))
 
         # Add the items to the scene. Items are stacked in the order they are added.
         self.scene.addItem(car)
@@ -122,13 +132,12 @@ class CarTrack(QWidget):
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
-        self.timer.start(100)
+        self.timer.start(17)
 
         hbox = QHBoxLayout(self)
         hbox.addWidget(view)
 
         self.setLayout(hbox)
-
 
     def update(self):
         for item in self.scene.items():
